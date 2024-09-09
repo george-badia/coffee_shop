f56087fe4cf232df1d90b3b3f6b219a1e5764ede
jojo <georgebadia2019@gmail.com>
2024-09-09 19:31:40 +0300

    Validate customer and coffee instances in Order class constructor

--- order.py
+++ order.py
@@ -7,7 +7,19 @@ class Order:
 
 #Initializing an Order instance with a customer, coffee, and price.the price here should be in float or convertible to float representing the price of the order (between 1.0 and 10.0)
     def __init__(self, customer, coffee, price):
-       
+   # Validating and raising valueError: If customer is not an instance of Customer or coffee is not an instance of Coffee.     
+ if not isinstance(customer, Customer):
+            raise ValueError("Invalid customer.")
+        if not isinstance(coffee, Coffee):
+            raise ValueError("Invalid coffee.")
+        
+        try:
+         #validating if the price is not a valid float between 1.0 and 10.0,if not raise value error
+            price = float(price)  
+            if not (1.0 <= price <= 10.0):
+                raise ValueError("Price must be between 1.0 and 10.0.")
+        except ValueError:
+            raise ValueError("Price must be a valid float between 1.0 and 10.0.")
 
         self._customer = customer
         self._coffee = coffee
